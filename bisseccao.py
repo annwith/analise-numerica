834f952050782f2564a38475a78715b8e3ff80b6
Juliana Midlej <jmesanto91@gmail.com>
2021-04-04 18:04:00 -0300

    update bissection function and delete main function

--- bisseccao.py
+++ bisseccao.py
@@ -1,69 +1,76 @@
 import math
 
+# Definir se existe 0 em um intervalo
+def intervalo_zero(funcao, a, b):
+    if funcao(a) * funcao(b) < 0:
+        return True
+    return False
+
 # Definir o número de casas de precisão pra fazer os cálculos
-def bisseccao(funcao, a, b, precisao_a=None, precisao_b=None, precisao_absoluta=None, precisao_relativa=None):
+def bisseccao(funcao, a, b, precisao_c=None, distancia_absoluta=None, distancia_relativa=None):
+    # Garantir a < b e a != b
+    if a > b:
+        [a, b] = [b, a]
+    elif a == b:
+        return "Interval Error"
+
     # Inicialização
+    it = []
+
     iteracao = 0
     f_a = funcao(a)
     f_b = funcao(b)
     d_absoluta = abs(a-b)
-    d_relativa = abs((a-b)/a)
+    if a != 0:
+        d_relativa = abs((a-b)/a)
+    else:
+        d_relativa = None
     
-    print(str(iteracao)+"\t"+str(a)+"\t"+str(b)+"\t"+str(f_a)+"\t"+str(f_b)+"\t"+str(d_absoluta)+"\t"+str(d_relativa))
+    it.append([iteracao, a, b, f_a, f_b, d_absoluta, d_relativa])
     
     repeat = False
-    if (precisao_absoluta != None and d_absoluta > precisao_absoluta):
+    if (distancia_absoluta != None and d_absoluta > distancia_absoluta):
         repeat = True
-    if (precisao_relativa != None and d_relativa > precisao_relativa):
+    if (a != 0 and distancia_relativa != None and d_relativa > distancia_relativa):
         repeat = True
-    if (precisao_a != None and abs(f_a) > precisao_a):
-        repeat = True
-    if (precisao_b != None and abs(f_b) > precisao_b):
+    if (precisao_c != None and abs(f_a) > precisao_c and abs(f_b) > precisao_c):
         repeat = True
 
     # Loop
-    while(repeat):
+    while(repeat and a < b):
         iteracao += 1
 
         # Escolher uma metade
-        meio = (a+b)/2
-        f_meio = funcao(meio)
+        c = (a+b)/2
+        f_c = funcao(c)
 
-        if f_a*f_meio < 0:
-            b = meio
+        if f_a*f_c < 0:
+            b = c
             f_b = funcao(b)
         else:
-            a = meio
+            a = c
             f_a = funcao(a)
         
         d_absoluta = abs(a-b)
-        d_relativa = abs((a-b)/a)
+        if a != 0:
+            d_relativa = abs((a-b)/a)
+        else:
+            d_relativa = None
 
-        print(str(iteracao)+"\t"+str(a)+"\t"+str(b)+"\t"+str(f_a)+"\t"+str(f_b)+"\t"+str(d_absoluta)+"\t"+str(d_relativa))
+        it.append([iteracao, a, b, f_a, f_b, d_absoluta, d_relativa])
 
         repeat = False
-        if (precisao_absoluta != None and d_absoluta > precisao_absoluta):
+        if (distancia_absoluta != None and d_absoluta > distancia_absoluta):
             repeat = True
-        if (precisao_relativa != None and d_relativa > precisao_relativa):
+        if (a != 0 and distancia_relativa != None and d_relativa > distancia_relativa):
             repeat = True
-        if (precisao_a != None and abs(f_a) > precisao_a):
+        if (precisao_c != None and abs(f_c) > precisao_c):
             repeat = True
-        if (precisao_b != None and abs(f_b) > precisao_b):
-            repeat = True
+    
+    return it
 
 # Definir o número de iterações necessárias
-def iteracoes_bisseccao(a, b, precisao_absoluta):
+def iteracoes_bisseccao(a, b, distancia_absoluta):
     i = abs(a-b)
-    n = (math.log(i)-math.log(precisao_absoluta))/math.log(2)
+    n = (math.log(i)-math.log(distancia_absoluta))/math.log(2)
     return n
-
-def main():
-    def funcao(x):
-        return 0.25*(x-2)+0.1*math.sin(x)
-
-    bisseccao(funcao, 1, 2, precisao_absoluta=0.001)
-
-    iteracoes = iteracoes_bisseccao(1, 2, precisao_absoluta=0.001)
-    print(math.ceil(iteracoes))
-
-main()
