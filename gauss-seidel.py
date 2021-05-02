f362552c39773c29b7d5b5c9a251f30e745e566b
Juliana Midlej <jmesanto91@gmail.com>
2021-05-01 23:37:06 -0300

    add gauss-seidel method to solve linear systems and fix jacobi return

--- gauss-seidel.py
+++ gauss-seidel.py
@@ -2,9 +2,9 @@ import numpy as np
 import math
 
 # TAMANHO MÍNIMO DO SISTEMA: 2 EQUAÇÕES
-def jacobi(m):
+def gauss_seidel(m):
     x1 = np.zeros_like(m[:, :-1])
-    np.fill_diagonal(x1, 0)
+    x2 = np.zeros_like(m[:, :-1])
     d1 = math.inf
     #print(x1)
     a = m[:, :-1]
@@ -14,13 +14,13 @@ def jacobi(m):
     parada = 0
     while True:
         #print(np.sum(a*x1, axis=1))
-        x2 = (b-np.sum(a*x1, axis=1))/np.diagonal(m)
-        x2 = np.repeat(np.reshape(x2, (1, x2.shape[0])), [x2.shape[0]], axis=0)
-        np.fill_diagonal(x2, 0)
+        for i in range(m.shape[0]):
+            x2[:, i] = (b[i]-np.sum(a[i, :]*x2[i, :]))/np.diagonal(m)[i]
+        
         print(x2)
+        np.fill_diagonal(x2, 0)
         d2 = np.absolute(x2-x1)
         print(d2)
-        # Variação absoluta
         if np.all(d2 < 0.001):
             x1 = np.copy(x2)
             break
@@ -42,5 +42,5 @@ m1 = [[3, -0.1, -0.2, -0.3, 7.85], [0.1, 7, -0.3, 0.3, -19.3], [0.3, -0.2, 10, 0
 m2 = [[3, -0.1, -0.2, 7.85], [0.1, 7, -0.3, -19.3], [0.3, -0.2, 10, 71.4]]
 m3 = [[0.1, 7, -19.3], [3, -0.2, 7.85]]
 m2 = np.asarray(m2)
-x = jacobi(m2)
+x = gauss_seidel(m2)
 print(x)
