f21bc40aa198a50416125ceec22a5470ebcc7705
Juliana Midlej <jmesanto91@gmail.com>
2021-06-15 23:47:35 -0300

    romberg integration

--- integracao/richards/richards.py
+++ integracao/richards/richards.py
@@ -5,7 +5,7 @@ def trapezios(pontos):
     # pontos ordenados por x
     x = pontos[:, 0]
     y = pontos[:, 1]
-    n = len(x)
+    p = len(x)
 
     if p == 1:
         print("Erro! Intervalo inválido!")
@@ -16,7 +16,7 @@ def trapezios(pontos):
     # calcular area
     i = 0
     # escolhi fazer assim pq funcionaria para um h diferente
-    for j in range(0, n-1):
+    for j in range(0, p-1):
         i += (y[j] + y[j+1])*abs(x[j] - x[j+1])/2
 
     return i
@@ -24,13 +24,19 @@ def trapezios(pontos):
 # aqui tem escolhas de implementação a serem feitas
 def richards(i1, h1, i2, h2):
     # garantir h1 > h2 (não sei ainda pq)
-    if h1 < h2:
-        h1, h2 = h2, h1
-        i1, i2 = i2, i1
+    # trocar por valor, não trocar os valores do vetor que estou passando
+    # pois a ordem faz diferença para construir a arvore
 
-    return i2 + (1/((h1/h2)**2-1)) * (i2-i1)
+    x1 = i1
+    x2 = i2
+    y1 = h1
+    y2 = h2
 
-print(richards(0.1728, 0.8, 1.0688, 0.4))
+    if y1 < y2:
+        y1, y2 = y2, y1
+        x1, x2 = x2, x1
+
+    return x2 + (1/((y1/y2)**2-1)) * (x2-x1)
 
 # n é um numero
 def funcao_pontos(funcao, a, b, segmentos):
@@ -39,7 +45,7 @@ def funcao_pontos(funcao, a, b, segmentos):
         a, b = b, a
 
     pontos = []
-    h = abs(a-b)/(segmentos+1)
+    h = abs(a-b)/(segmentos)
 
     pontos.append([a, solve_func(a, funcao)])
     for i in range(1, segmentos):
@@ -49,19 +55,36 @@ def funcao_pontos(funcao, a, b, segmentos):
     return pontos, h
 
 # n é uma lista de numeros de pontos
-def arvore_richards(funcao, a, b, n):
-    r = []
-    for i in range(len(n)):
-        r.append(funcao_pontos(funcao, a, b, n[i])) # retorna [pontos, h]
-
-    print(r)
-
-    # inicialização com um método de integração
-    for i in range(len(r)):
-        r[i][0] = trapezios(r[i][0])
-
-    print(r)
+# ou seria a integração de romberg (reaproveitamento de richards)
+def arvore_richards(funcao, a, b, lista_segmentos):
+    r = np.zeros((len(lista_segmentos), len(lista_segmentos)))
+    h = []
+    for i in range(len(lista_segmentos)):
+        pontos, distancia = funcao_pontos(funcao, a, b, lista_segmentos[i]) # retorna [pontos, h]
+        r[i][0] = trapezios(np.asarray(pontos)) # passa os pontos para uma funcao de integração)
+        h.append(distancia)
+ 
+    print(h)
+    print(np.asarray(r))
 
     # finalizar construção da arvore
+    for i in range(1, r.shape[0]):
+        for j in range(r.shape[0]-i):
+            r[j][i] = richards(r[j][i-1], h[j], r[j+1][i-1], h[j+i])
 
-arvore_richards()
+    print(r)
+
+    return r, h
+
+# faz a melhor integração possível dados os pontos
+# def integracao():
+
+# adiciona mais um h na árvore
+# def add_arvore(arvore, i, h):
+
+funcao = "0.2 + 25*x - 200*x^2 + 675*x^3 - 900*x^4 + 400*x^5"
+arvore_richards(funcao, 0, 0.8, [1, 2, 4, 8])
+
+# a ordem dos segmentos importa?
+# é possível tirar e colocar segmentos? se sim só da ultima posicao, como uma pilha correto?
+# o tipo de integração importa? eu posso fazer simpson com trapézio e tudo bem?
