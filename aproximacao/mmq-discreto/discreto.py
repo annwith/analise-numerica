df754fe4ede49fa00d525d641927075ce0813664
Juliana Midlej <jmesanto91@gmail.com>
2021-06-17 10:35:50 -0300

    finalizar mmq discreto

--- aproximacao/mmq-discreto/discreto.py
+++ aproximacao/mmq-discreto/discreto.py
@@ -3,14 +3,50 @@ Aproximação Polinomial
 Método dos Mínimos Quadrados
 '''
 import numpy as np
+import matplotlib.pyplot as plt
+
+def eliminacao_gauss(m):
+    for i in range(0, m.shape[0]-1):
+        sub = m[i+1:, i] / m[i][i]
+        for j in range(i, m.shape[0]-1):
+            m[j+1, :] = m[j+1, :] - sub[j-i]*m[i, :]
+
+    x = []
+    for i in range(m.shape[0]-1, -1, -1):
+        x.append((m[i][-1] - np.sum(m[i, i+1:-1])) / m[i][i])
+        m[:, i] *= x[-1]
+
+    return np.flip(x)
+
+def mostrar_grafico(pontos, a):
+    # Data for plotting
+    pontos = np.asarray(pontos)
+    x = pontos[:, 0]
+    y = pontos[:, 1]
+
+    # Data for plotting
+    xl = np.arange(x[0], x[-1], 0.01)
+    yl = np.zeros_like(xl)
+    for i in range(xl.shape[0]):
+        aux = 0
+        for j in range(a.shape[0]):
+            aux += a[j]*xl[i]**(j)
+        yl[i] = aux
+
+    fig, ax = plt.subplots()
+    ax.plot(x, y, 'o')
+    ax.plot(xl, yl)
+
+    ax.set(xlabel='x', ylabel='y')
+    ax.grid()
+
+    fig.savefig("discreto.png")
+    plt.show()
 
 def discreto(pontos, m):
     pontos = np.asarray(pontos)
     x = pontos[:, 0]
     y = pontos[:, 1]
-
-    print(x)
-    print(y)
 
     # Base vetorial
     p = []
@@ -18,9 +54,9 @@ def discreto(pontos, m):
         p.append(x**i)
 
     p = np.asarray(p)
-    print(p)
+    # print(p)
 
-    matriz = np.zeros((m+1, m+1))
+    matriz = np.zeros((m+1, m+2))
     ly = np.zeros((m+1))
 
     for i in range(m+1):
@@ -28,8 +64,20 @@ def discreto(pontos, m):
         for j in range(m+1):
             matriz[i][j] = np.sum(p[i]*p[j])
 
-    print(matriz)
-    print(ly)
+    # print(matriz)
+    # print(ly)
+
+    for i in range(ly.shape[0]):
+        matriz[i][-1] = ly[i]
+
+    # print(matriz)
+
+    a = eliminacao_gauss(matriz)
+    # print(a)
+
+    return a
+
+    # testar quão os coeficientes estão bons
 
     # Resolver esse sistema linear
     # Gauss, LU, Jacobi, Gauss-Seidel
@@ -39,6 +87,43 @@ def discreto(pontos, m):
 
     # Testar o resultado final
 
-pontos = [[-1, 0], [0, -1], [1, 0], [2, 7]]
+def predicao(a, x):
+    y = 0
+    for j in range(a.shape[0]):
+        y += a[j]*x**(j)
+    return y
 
-discreto(pontos, 2)
+def main():
+    input_file = open('input.txt', 'r')
+    output_file = open('output.txt', 'w')
+    pontos = []
+    predict = False
+    for l in input_file:
+            l = l.split()
+            l = [float(i) for i in l]
+            if len(l) == 1:
+                m = int(l[0])
+            elif len(l) == 2:
+                pontos.append(l)
+            # predicao
+            elif len(l) > 2:
+                predict = True
+                a = l
+    
+    if predict:
+        a = np.asarray(a)
+        y = predicao(a[:-1], a[-1])
+        output_file.write(str(y))
+    else:
+        pontos = np.asarray(pontos)
+        a = discreto(pontos, m)
+
+        for i in range(a.shape[0]):
+            output_file.write("a"+str(i)+" = "+str(a[i])+"\n")
+
+        mostrar_grafico(pontos, a)
+    
+    input_file.close()
+    output_file.close()
+
+main()
