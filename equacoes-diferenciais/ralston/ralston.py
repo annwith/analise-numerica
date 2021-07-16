ec9119875a6cc815da9ff686ba401fc2aae1022e
Juliana Midlej <jmesanto91@gmail.com>
2021-07-16 01:03:09 -0300

    ralston update

--- equacoes-diferenciais/ralston/ralston.py
+++ equacoes-diferenciais/ralston/ralston.py
@@ -17,6 +17,41 @@ def ralston(funcao, x_i, x_f, y_i, h):
 
     return pontos
 
+def solve_system_ralston(functions, variables, values, x_f, h):
+    pontos = []
+    for value in values:
+        pontos.append(value)
+    
+    # constantes
+    k = np.zeros((4, len(variables)-1))
+    
+    while(values[0] < x_f):
+        aux = []
+        aux.append(values[0])
+        # definir constantes
+        for i in range(len(variables)-1):
+            k[0][i] = h*solve_diferential_function(variables, values, functions[i])
+        aux[0] = values[0]+h*(3/4)
+        for j in range(len(variables)-1):
+            aux.append(values[j+1]+k[0][j]*(3/4))
+        for i in range(len(variables)-1):
+            k[1][i] = h*solve_diferential_function(variables, aux, functions[i])
+        
+        for i in range(len(variables)-1):
+            aux[i+1] = values[i+1] + ((1/3)*k[0][i]+(2/3)*k[1][i])
+        aux[0] = values[0]+h
+
+        # só atualiza os valores juntos
+        for i in range(len(aux)):
+            values[i] = aux[i]
+        for value in values:
+            pontos.append(value)
+    
+    pontos = np.asarray(pontos)
+    pontos = np.reshape(pontos, (int(len(pontos)/len(values)), len(values)))
+
+    return pontos
+
 def mostrar_grafico(pontos):
     pontos = np.asarray(pontos)
     # Data for plotting
